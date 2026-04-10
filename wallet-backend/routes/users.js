62d1c0095b27c1b9a692b0da90699fdf7592113b
Bhavya Singhal <bhanusinghal@Bhavyas-MacBook-Air.local>
2026-04-10 16:14:12 +0530

    replacing errors with issues in signin api

--- wallet-backend/routes/users.js
+++ wallet-backend/routes/users.js
@@ -79,7 +79,7 @@ UserRouter.post("/signin",async function(req,res){
             })
         }
     }else{
-        res.status(403).json(isdatasafe.error)
+        res.status(403).json(isdatasafe.issues)
     }
 })
 
